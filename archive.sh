f798985958e4cd33785f22135f538c70efdb966f
Bego Mario Garde <pixolin@pixolin.de>
2022-02-20 13:29:51 +0100

    simplifies archive.sh

--- archive.sh
+++ archive.sh
@@ -13,21 +13,14 @@
 # check if site name provided,
 # check if directory exists,
 #
-# create temporary files with
-# list of themes and plugins,
-# export database as sql file,
-#
+# dump MySQL database
 # create tar with
+# - `database.sql`
 # - `wp-config.php`
 # - `.htaccess`
-# - `wp-content/uploads`
-# - temporary file with list of active plugins
-# - temporary file with list of active themes
-# - sql file of database
-# - SSL certificates (both!),
-# - virtual host files
+# - `wp-content`
+# store under /var/archive
 #
-# Delete temporary files after storage.
 
 # Exit on first error
 set -e
@@ -57,42 +50,14 @@ if [[ ! -d "$DIR" ]]; then
   exit 1
 fi
 
-# temporary hidden folder in website directory
-sudo -u www-data mkdir -p "${TMP}"
-
 # export MySQL database and store in tmp-directory
-$WWWP db export "${TMP}"/wp-database.sql --dbuser=wordpress --dbpass=wordpress --path="${DIR}"
-
-# Store list of themes and plugins in tmp-folder
-$WWWP theme list --field=name --status=active --skip-update-check --path="${DIR}" >"${TMP}"/wp-theme.txt
-$WWWP plugin list --field=name --status=active --skip-update-check --path="${DIR}" >"${TMP}"/wp-plugins.txt
+$WWWP db export "${DIR}"/database.sql --dbuser=wordpress --dbpass=wordpress --path="${DIR}"
 
 # Create archive from files in tmp-directory
-tar -C "${TMP}" -cf "${TAR}" \
+tar -C "${DIR}" -cfz "${TAR}" "${TMP}" \
   wp-database.sql \
-  wp-plugins.txt \
-  wp-theme.txt
-
-# tmp-directory is no longer needed
-rm -rf "${TMP}"
-
-# Add `wp-config.php`, `.htaccess` and uploads-directory to archive
-tar -C "${DIR}" -rf "${TAR}" \
   wp-config.php \
   .htaccess \
-  wp-content/uploads
-
-# Add SSL-Certificates and Virtual Hosts to archive
-tar -C /etc -rf "${TAR}" \
-  ssl/certs/"${SITE}".pem \
-  ssl/private/"${SITE}".key \
-  apache2/sites-available/"${SITE}".conf \
-  apache2/sites-available/"${SITE}".ssl.conf
-
-# Compress archive with gzip, remove original file
-gzip "${TAR}"
-
-# Change permissions of archive
-chmod 600 "${TAR}".gz
+  wp-content
 
 echo "✅ Compressed archive stored at /var/archive/${SITE}.gz"
